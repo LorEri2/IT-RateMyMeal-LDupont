cde3ac72e4a3c3d9cc821465b9472f995fd63919
loris <loris.dupont@condorcet.be>
2025-12-05 15:43:26 +0100

    Fix : Bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
-import AfficherPlat from '../components/AfficherPlat';
+import AfficherPlat from '@/components/AfficherPlat';
 
 export default function Index() {
   return (
@@ -8,21 +8,21 @@ export default function Index() {
       
       <Text style={styles.headerTitle}>Mes Repas Notés</Text>
 
-      {/* Plat 1 : Pizza */}
+      
       <AfficherPlat 
         nom="Pizza Margherita" 
         note={8} 
         image="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=800&auto=format&fit=crop" 
       />
 
-      {/* Plat 2 : Burger */}
+      
       <AfficherPlat 
         nom="Burger Maison" 
         note={9} 
         image="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?q=80&w=800&auto=format&fit=crop" 
       />
 
-      {/* Plat 3 : Pâtes */}
+     
       <AfficherPlat 
         nom="Pâtes Carbonara" 
         note={6} 
@@ -35,15 +35,15 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 50, // Un peu d'espace en haut
+    paddingVertical: 50, 
     paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center', // Centre tout horizontalement
+    alignItems: 'center', 
   },
   headerTitle: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 30, // Espace entre le titre et le premier plat
+    marginBottom: 30, 
     color: '#333',
   },
 });
